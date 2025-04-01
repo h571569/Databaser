6a66900c01adb4a2d558cac55d78de8c39a990ac
h571569 <571569@stud.hvl.no>
2025-04-01 16:40:24 +0200

    auto

--- src/main/resources/AnsattDB.sql
+++ src/main/resources/AnsattDB.sql
@@ -6,25 +6,29 @@ SET search_path TO oblig3;
 
 CREATE TABLE avdeling (
 
-     avdelingId   SERIAL primary key,
-     avdelingNavn varchar(50),
-     sjefId     int not null
+                          avdelingId   SERIAL primary key,
+                          avdelingNavn varchar(50),
+                          sjefId     int
 );
 
 CREATE TABLE ansatt (
 
-    ansattId   SERIAL primary key,
-    brukernavn char(4) unique not null,
-    fornavn    varchar(50),
-    etternavn  varchar(50),
-    ansdato    date,
-    stilling   varchar(50),
-    manedslonn int,
-    avdelingId int not null,
-    foreign key (avdelingId) references avdeling(avdelingId)
+                        ansattId   SERIAL primary key,
+                        brukernavn char(4) unique not null,
+                        fornavn    varchar(50),
+                        etternavn  varchar(50),
+                        ansdato    date,
+                        stilling   varchar(50),
+                        manedslonn int,
+                        avdelingId int,
+                        foreign key (avdelingId) references avdeling(avdelingId)
 );
 
-alter table avdeling add foreign key (sjefId) references ansatt(ansattId);
+INSERT INTO
+    avdeling(avdelingNavn)
+values
+    ('IT-Avdeling'),
+    ('WebDev-Avdeling');
 
 INSERT INTO
     ansatt(brukernavn, fornavn, etternavn, ansdato, stilling, manedslonn, avdelingId)
@@ -36,23 +40,15 @@ values
     ('gaby','gabriel','hugaas','2019-09-30','seniordev',95000,2),
     ('elif','elias','frette','2023-04-07','developer',70000,2);
 
-
-
-INSERT INTO
-    avdeling(avdelingNavn,sjefId)
-values
-    ('IT-Avdeling',1),
-    ('WebDev-Avdeling',5);
-
--- Man kan gjøre dette for å sette sjef i tabellen, men hvis avdeling MÅ ha en sjef så kan man like gjerne sette det inn med en gang.
--- UPDATE avdeling set sjefId = 1 where avdelingId = 1;
--- UPDATE avdeling set sjefId = 5 where avdelingId = 2;
+UPDATE avdeling set sjefId = 1 where avdelingId = 1;
+UPDATE avdeling set sjefId = 5 where avdelingId = 2;
+alter table avdeling add foreign key (sjefId) references ansatt(ansattId);
 
 CREATE TABLE prosjekt (
 
-    prosjektId   SERIAL primary key,
-    prosjektNavn varchar(50),
-    beskrivelse  TEXT
+                          prosjektId   SERIAL primary key,
+                          prosjektNavn varchar(50),
+                          beskrivelse  TEXT
 );
 
 INSERT INTO
@@ -64,12 +60,13 @@ values
 
 CREATE TABLE prosjektDeltagelse (
 
-    ansattId     int, primary key,
-    prosjektId   int, primary key,
-    rolle        varchar(50),
-    arbeidstimer int,
-    foreign key (ansattId) references ansatt(ansattId),
-    foreign key (prosjektId) references prosjekt(prosjektId)
+                                    ansattId     int,
+                                    prosjektId   int,
+                                    rolle        varchar(50),
+                                    arbeidstimer int,
+                                    foreign key (ansattId) references ansatt(ansattId),
+                                    foreign key (prosjektId) references prosjekt(prosjektId),
+                                    primary key (ansattId, prosjektId)
 );
 
 INSERT INTO prosjektDeltagelse
